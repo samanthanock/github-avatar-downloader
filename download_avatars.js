96bb49f5ec272ed3ced95d503ed28199217d9119
Samantha <sammarienock@gmail.com>
2019-01-15 03:36:57 +0000

    makes accessible

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,21 @@
 var request = require('request');
+var token = require('./secrets.js');
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url:
+      'https://api.github.com/repos/' +
+      repoOwner +
+      '/' +
+      repoName +
+      '/contributors',
+    headers: {
+      'User-Agent': 'LHL Assingment',
+    },
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
