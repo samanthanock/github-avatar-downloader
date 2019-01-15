f2150c6ff75251bd4400790401be199c65f714e7
Samantha <sammarienock@gmail.com>
2019-01-15 05:12:23 +0000

    updates current code

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,14 @@
+// there are the require files
 var request = require('request');
 var token = require('./secrets.js');
+var fs = require('fs');
+
+// implementing commant line variables
+var commandOne = process.argv[2];
+var commandTwo = process.argv[3];
+
+console.log('Welcome to the GitHub Avatar Downloader!'); // logs a welcome message
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url:
@@ -9,13 +18,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
       repoName +
       '/contributors',
     headers: {
-      'User-Agent': 'LHL Assingment',
+      'User-Agent': 'request',
+      Authorization: 'token ' + secrets.GITHUB_TOKEN,
     },
   };
 
+  // If the user does not specify both arguments,
+  // the program should not attempt a request.
+  // It should instead terminate with an error message letting the user know about the problem.
+  if (commandOne === undefined || commandTwo === undefined) {
+    console.log('There is an error with your arguments!');
+    return false;
+  }
+
   request(options, function(err, res, body) {
-    cb(err, body);
+    var data = JSON.parse(body);
+    data.forEach(function(element) {
+      cb(err, body);
+    });
   });
+}
+
+function downloadImageByURL(url, filePath) {
+  request(url).pipe(fs.createWriteStream(filePath));
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
