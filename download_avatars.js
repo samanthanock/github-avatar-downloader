1a7ec92e51526ea4319cdd162c4bc75ef017b24b
Samantha <sammarienock@gmail.com>
2019-01-15 03:19:21 +0000

    adds onto function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,9 @@
 var request = require('request');
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+getRepoContributors('jquery', 'jquery', function(err, result) {
+  console.log('Errors:', err);
+  console.log('Result:', result);
+});
